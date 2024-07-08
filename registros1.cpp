664f33da74ac1d81036dc1fb450f7abc6012cb97
arramosm13 <arramosm@unjbg.edu.pe>
2024-07-08 11:31:40 -0500

    agregue para que saliera lo que pidieran.

--- registros1.cpp
+++ registros1.cpp
@@ -4,17 +4,28 @@ using namespace std;
 struct datos_empleado{
 	char nombre[30];
 	char sexo[2];
-	double sueldo[5000];
+	int sueldo;
 } empresaDataTech;
 int main(){
 	datos_empleado  empleado[30];
-	double n, mayor, menor, i;
+	double n, mayor, menor;
 	cout<<"Ingrese el numero de empleados: "; cin>>n;
-	for (i=0; i<n; i++){
+	for (int i=0; i<n; i++){
 	cout<<"Ingrese el nombre del empleado: "; cin>>empleado[i].nombre;
 	cout<<"Ingrese el sexo del empleado: "; cin>>empleado[i].sexo;
-	cout<<"Ingrese el salario del empleado: "; cin>>empleado[i].salario;
+	cout<<"Ingrese el salario del empleado: "; cin>>empleado[i].sueldo;
 	}
-	
-
+	menor=0;
+	for(int i=0; i<n; i++){
+		if (menor<empleado[i].sueldo){
+			menor=empleado[i].sueldo;
+		}cout<<"El empleado con menor sueldo es:"<<empleado[i].sueldo<<endl;
+	} 
+	mayor=0;
+	for(int i=0; i<n; i++){
+		if (mayor>empleado[i].sueldo){
+			mayor=empleado[i].sueldo;
+		}cout<<"El empleado con mayor sueldo es:"<<empleado[i].sueldo<<endl;
+	} 
+return 0;
 }
