b1adb5ab1af69bff02550141258b64352c7d3564
arramosm13 <arramosm@unjbg.edu.pe>
2024-07-13 16:25:43 -0500

    En el ejercicio 3 termine agregando un for, un if dentro de el y otro afuera para lograr que el programa devuelva si el numero del mes ingresado coincide con alguno.

--- registros3.cpp
+++ registros3.cpp
@@ -11,20 +11,30 @@ struct datos_persona {
 
 int main() {
     datos_persona persona[40];
-    int n, mes_buscar;
+    int n, mes_buscar, coincidencia=0;
     cout << "Ingrese el numero de personas que desea saber sus datos: "; cin >> n;
 	for (int i = 0; i < n; i++) {
         cout << "Ingrese el nombre de la persona: "; cin >> persona[i].nombre;
         cout << "Ingrese el dia de nacimiento: "; cin >> persona[i].dia;
         cout << "Ingrese el mes de nacimiento: "; cin >> persona[i].mes;
-        cout << "Ingrese el año de nacimiento: "; cin >> persona[i].tiempo;
+        cout << "Ingrese el ano de nacimiento: "; cin >> persona[i].tiempo;
         cout << "--------------------------------\n";
     }
     
     do {
         cout << "Ingrese un numero de mes (la opcion 0 es para salir): "; cin >> mes_buscar;
-
+		cout << "Las personas que cumplen anos en el mes " << mes_buscar << " son:" << endl;
+        for (int i = 0; i < n; i++) {
+            if (persona[i].mes == mes_buscar) {
+                cout << "Nombre: " << persona[i].nombre << ", Día: " << persona[i].dia << ", Año: " << persona[i].tiempo << endl;
+                coincidencia++;
+            }
+        }
+        if (coincidencia == 0) {
+            cout << "Ninguna persona cumple anos en el mes " << mes_buscar << "." << endl;
+        }
     } while (mes_buscar != 0);
+    cout << "Se cerrara el programa." << endl;
     return 0;
 }
 
