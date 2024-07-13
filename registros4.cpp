d10b509a6f178a3c673787278e45db410901eaf9
arramosm13 <arramosm@unjbg.edu.pe>
2024-07-13 18:33:04 -0500

    En el ejercicio 4 para finalizar se le pide al usuario ingresar un pais, luego este pasa por un for y algunos if, para comparar las medallas del pais ingresado se uso un strcmp de la libreria string, y bueno con eso devuelve el atleta con mayor medallas de ese pais y si ingresa un pais que no esta en los datos anteriores, bota que no hay ningun atleta registrado en ese pais.

--- registros4.cpp
+++ registros4.cpp
@@ -13,14 +13,31 @@ int main() {
     datos_atleta atleta[20];
     int n;
 	char pais_buscar[30];
-    cout << "Ingrese el número de atletas que desea saber su informacion: "; cin >> n;
+    cout << "Ingrese el numero de atletas que desea saber su informacion: "; cin >> n;
     for (int i = 0; i < n; i++) {
         cout << "Ingrese el nombre del atleta: "; cin >> atleta[i].nombre;
-        cout << "Ingrese el país del atleta: "; cin >> atleta[i].pais;
+        cout << "Ingrese el pais del atleta: "; cin >> atleta[i].pais;
         cout << "Ingrese la disciplina del atleta: "; cin >> atleta[i].disciplina;
-        cout << "Ingrese el número de medallas que tiene el atleta: "; cin >> atleta[i].medallas;
-        cout << "--------------------------------\n";
+        cout << "Ingrese el numero de medallas que tiene el atleta: "; cin >> atleta[i].medallas;
+        cout << "------------------------------------------------------------------\n";
     }
-    cout << "Ingrese el nombre del país que desea buscar: "; cin >> pais_buscar;
+    cout << "Ingrese el nombre del pais que desea buscar: "; cin >> pais_buscar;
+    int max_medallas = -1;
+    int maxmeda_buscar = -1;
+    cout << "Los atletas ingresados que son de " << pais_buscar << " son:" << endl;
+    for (int i = 0; i < n; i++) {
+        if (strcmp(atleta[i].pais, pais_buscar) == 0) { 
+            cout << "Nombre: " << atleta[i].nombre << ", Disciplina: " << atleta[i].disciplina << ", Numero de medallas: " << atleta[i].medallas << endl;
+            if (atleta[i].medallas > max_medallas) {
+                max_medallas = atleta[i].medallas;
+                maxmeda_buscar = i;
+            }
+        }
+    }
+    if (maxmeda_buscar != -1) { 
+        cout << "El atleta que tiene el mayor numero de medallas de " << pais_buscar << " es: "<< atleta[maxmeda_buscar].nombre << " con " << atleta[maxmeda_buscar].medallas << " medallas." << endl;
+    } else {
+        cout << "No hay ningun atleta registrado de " << pais_buscar << "." << endl;
+    }
     return 0;
 }
