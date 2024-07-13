00dcbcb669e53b98b94c188602c43d6492ca4938
arramosm13 <arramosm@unjbg.edu.pe>
2024-07-13 12:54:42 -0500

    En el ejercicio 3 agregue un for y asi pueda pedir ingresar los datos de la personas.

--- registros3.cpp
+++ registros3.cpp
@@ -13,7 +13,13 @@ int main() {
     datos_persona persona[40];
     int n;
     cout << "Ingrese el numero de personas que desea saber sus datos: "; cin >> n;
-
+	for (int i = 0; i < n; i++) {
+        cout << "Ingrese el nombre de la persona: "; cin >> persona[i].nombre;
+        cout << "Ingrese el dia de nacimiento: "; cin >> persona[i].dia;
+        cout << "Ingrese el mes de nacimiento: "; cin >> persona[i].mes;
+        cout << "Ingrese el año de nacimiento: "; cin >> persona[i].tiempo;
+        cout << "--------------------------------\n";
+    }
     return 0;
 }
 
