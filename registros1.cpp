a79d76432529c54c6e1b4f1efc3142e174c49a1c
arramosm13 <arramosm@unjbg.edu.pe>
2024-07-13 12:09:09 -0500

    En el ejercicio 1 arregle la parte que definia cual era el sueldo mayor y el menor, para que solo devuelva un resultado x cada uno.

--- registros1.cpp
+++ registros1.cpp
@@ -8,24 +8,29 @@ struct datos_empleado{
 } empresaDataTech;
 int main(){
 	datos_empleado  empleado[30];
-	double n, mayor, menor;
+	int n;
+	double mayor, menor;
+	
 	cout<<"Ingrese el numero de empleados: "; cin>>n;
 	for (int i=0; i<n; i++){
 	cout<<"Ingrese el nombre del empleado: "; cin>>empleado[i].nombre;
 	cout<<"Ingrese el sexo del empleado: "; cin>>empleado[i].sexo;
 	cout<<"Ingrese el salario del empleado: "; cin>>empleado[i].sueldo;
 	}
-	menor=0;
-	for(int i=0; i<n; i++){
-		if (menor<empleado[i].sueldo){
-			menor=empleado[i].sueldo;
-		}cout<<"El empleado con menor sueldo es:"<<empleado[i].sueldo<<endl;
-	} 
-	mayor=0;
-	for(int i=0; i<n; i++){
-		if (mayor>empleado[i].sueldo){
-			mayor=empleado[i].sueldo;
-		}cout<<"El empleado con mayor sueldo es:"<<empleado[i].sueldo<<endl;
-	} 
+	menor = empleado[0].sueldo;
+    for (int i = 0; i < n; i++) {
+        if (empleado[i].sueldo < menor) {
+            menor = empleado[i].sueldo;
+        }
+    }
+    mayor = empleado[0].sueldo;
+    for (int i = 0; i < n; i++) {
+        if (empleado[i].sueldo > mayor) {
+            mayor = empleado[i].sueldo;
+        }
+	}
+	cout << "El empleado con menor sueldo es: " << menor << endl;
+    cout << "El empleado con mayor sueldo es: " << mayor << endl;
+ 
 return 0;
 }
