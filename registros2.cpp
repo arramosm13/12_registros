0a275b32b8cb19d472a85d5c01034a455e73bd9f
arramosm13 <arramosm@unjbg.edu.pe>
2024-07-12 23:37:42 -0500

    En el ejercicio 2 agregue un contador para las personas que son mayores a 50 años, y devuleve el numero de personas con esa caracteristica.

--- registros2.cpp
+++ registros2.cpp
@@ -12,12 +12,16 @@ int main() {
 	persona persona[40];
     int n;
     cout << "Ingrese el numero de personas: "; cin >> n;
-    
+    int mayores50 = 0;
     
     for (int i = 0; i < n; i++) {
         cout << "Ingrese el nombre de la persona: "; cin>>persona[i].nombre;
         cout << "Ingrese el DNI de la persona: "; cin>>persona[i].DNI;
         cout << "Ingrese la edad de la persona: "; cin >> persona[i].edad;
-}
+    if (persona[i].edad > 50) {
+            mayores50++;
+        }
+    }
+    cout << "Hay "<< mayores50 <<" personas mayores a 50 años. " << endl;
 return 0;
 }
