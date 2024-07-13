4344c4b75fe45ae0f3ccf59283dfb660e5d89561
arramosm13 <arramosm@unjbg.edu.pe>
2024-07-12 23:50:42 -0500

    En el ejercicio 2 agregue una operacion dentro del for, que es un acumulador de las edades de todas las personas ingresadas, para luego hallar el promedio, que es lo que devuelve.

--- registros2.cpp
+++ registros2.cpp
@@ -13,6 +13,7 @@ int main() {
     int n;
     cout << "Ingrese el numero de personas: "; cin >> n;
     int mayores50 = 0;
+    double suma_edades = 0, promedio_edades;
     
     for (int i = 0; i < n; i++) {
         cout << "Ingrese el nombre de la persona: "; cin>>persona[i].nombre;
@@ -21,7 +22,10 @@ int main() {
     if (persona[i].edad > 50) {
             mayores50++;
         }
+    suma_edades += persona[i].edad;
     }
-    cout << "Hay "<< mayores50 <<" personas mayores a 50 años. " << endl;
+    promedio_edades = suma_edades / n;
+    cout << "Hay "<< mayores50 <<" personas mayores a 50 años. \n" << endl;
+    cout << "El promedio de todas las edades de las personas es: " << promedio_edades << endl;
 return 0;
 }
